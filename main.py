bf59d2d315e32881f2393a3a2498955efe5cff96
Julien Nyambal <julien.nyambal@gmail.com>
2026-03-25 22:45:01 +0200

    new upload with error on vehicle

---
+++ main.py
@@ -0,0 +1,11 @@
+import vehicle as vh
+
+
+car_1 = vh.Vehicle("Benz")
+car_1.drive()
+
+car_2 = vh.Vehicle("BMW")
+car_2.drive()
+
+car_3 = vh.Vehicle("Toyota")
+car_3.drive() 
---
+++ vehicle.py
@@ -0,0 +1,5 @@
+class Vehicle: 
+    def __int__(self, name):
+        self.name = name
+    def drive(self):
+        print("Drive: ", self.name)
